5d287c2c3cea51f45efdb5816424b79293914429
lucas <Pulsar-kkaturi>
2021-07-29 11:27:09 +0900

    [Ver.0.0.2] VGG16_test update (2021.07.29.11:27)

--- CNN/VGG16_test.py
+++ CNN/VGG16_test.py
@@ -18,47 +18,7 @@ from tensorflow.keras import metrics
 from tensorflow.keras import regularizers
 from tensorflow.keras import utils
 
-################
-# 2. Data Load #
-################
-# tf.debugging.set_log_device_placement(True)
-(x_train, y_train), (x_test, y_test)=keras.datasets.mnist.load_data(path='minist.npz')
-print(x_train.shape, y_train.shape)
 
-x_train_list = []
-x_test_list = []
-for i, i_ in enumerate(x_train[:1000]):
-    arr = np.zeros(shape=(32, 32))
-    arr[:28,:28] = x_train[i]
-    x_train_list.append(arr)
-for i, i_ in enumerate(x_test[:500]):
-    arr = np.zeros(shape=(32, 32))
-    arr[:28,:28] = x_test[i]
-    x_test_list.append(arr)
-
-x_train1 = np.expand_dims(np.array(x_train_list), axis=-1)
-x_test1 = np.expand_dims(np.array(x_test_list), axis=-1)
-print('Image Shape = ' ,x_train1.shape, x_test1.shape)
-
-y_train_list = []
-y_test_list = []
-for i, i_ in enumerate(y_train[:1000]):
-    zero = [0] * 10
-    zero[i_] = 1
-    y_train_list.append(zero)
-
-for i, i_ in enumerate(y_test[:500]):
-    zero = [0] * 10
-    zero[i_] = 1
-    y_test_list.append(zero)
-
-y_train1 = np.array(y_train_list)
-y_test1 = np.array(y_test_list)
-print('Label Shape', y_train1.shape, y_test1.shape)
-
-#################
-# Network Build #
-#################
 def conv_block_2d(lr_conv, lr_num, par_list):
     # parameter
     conv_size = par_list[0]
@@ -75,6 +35,7 @@ def conv_block_2d(lr_conv, lr_num, par_list):
     lr_pool = layers.MaxPooling2D(pool_size=pool_str)(lr_conv)
     return lr_pool
 
+
 def output_block(lr_dense, block_num, flat_count, reg_weight, act_func, drop_rate):
     lr_dense = layers.Flatten()(lr_dense)
     lr_dense = layers.Dropout(drop_rate)(lr_dense)
@@ -86,22 +47,63 @@ def output_block(lr_dense, block_num, flat_count, reg_weight, act_func, drop_rat
         lr_dense = layers.Dropout(drop_rate)(lr_dense)
     return lr_dense
 
-inputs = Input(shape=(32,32,1))
-block1 = conv_block_2d(inputs, 2, [64, 3, 'relu', 2, None])
-block2 = conv_block_2d(block1, 2, [128, 3, 'relu', 2, None])
-block3 = conv_block_2d(block2, 3, [256, 3, 'relu', 2, None])
-block4 = conv_block_2d(block3, 3, [512, 3, 'relu', 2, None])
-block5 = conv_block_2d(block4, 3, [512, 3, 'relu', 2, None])
-dens = output_block(block5, 1, [256], None, 'relu', 0.5)
-outputs = layers.Dense(10, activation='softmax')(dens)
-model = Model(inputs, outputs)
-model.summary()
-
-###############
-# Train Model #
-###############
+################
+# 2. Data Load #
+################
+# tf.debugging.set_log_device_placement(True)
 os.environ['CUDA_VISIBLE_DEVICES'] = '1,2,3'
 strategy = tf.distribute.MirroredStrategy()
 with strategy.scope():
+    (x_train, y_train), (x_test, y_test)=keras.datasets.mnist.load_data(path='minist.npz')
+    print(x_train.shape, y_train.shape)
+
+    x_train_list = []
+    x_test_list = []
+    for i, i_ in enumerate(x_train[:1000]):
+        arr = np.zeros(shape=(32, 32))
+        arr[:28,:28] = x_train[i]
+        x_train_list.append(arr)
+    for i, i_ in enumerate(x_test[:500]):
+        arr = np.zeros(shape=(32, 32))
+        arr[:28,:28] = x_test[i]
+        x_test_list.append(arr)
+
+    x_train1 = np.expand_dims(np.array(x_train_list), axis=-1)
+    x_test1 = np.expand_dims(np.array(x_test_list), axis=-1)
+    print('Image Shape = ' ,x_train1.shape, x_test1.shape)
+
+    y_train_list = []
+    y_test_list = []
+    for i, i_ in enumerate(y_train[:1000]):
+        zero = [0] * 10
+        zero[i_] = 1
+        y_train_list.append(zero)
+
+    for i, i_ in enumerate(y_test[:500]):
+        zero = [0] * 10
+        zero[i_] = 1
+        y_test_list.append(zero)
+
+    y_train1 = np.array(y_train_list)
+    y_test1 = np.array(y_test_list)
+    print('Label Shape', y_train1.shape, y_test1.shape)
+
+    #################
+    # Network Build #
+    #################
+    inputs = Input(shape=(32,32,1))
+    block1 = conv_block_2d(inputs, 2, [64, 3, 'relu', 2, None])
+    block2 = conv_block_2d(block1, 2, [128, 3, 'relu', 2, None])
+    block3 = conv_block_2d(block2, 3, [256, 3, 'relu', 2, None])
+    block4 = conv_block_2d(block3, 3, [512, 3, 'relu', 2, None])
+    block5 = conv_block_2d(block4, 3, [512, 3, 'relu', 2, None])
+    dens = output_block(block5, 1, [256], None, 'relu', 0.5)
+    outputs = layers.Dense(10, activation='softmax')(dens)
+    model = Model(inputs, outputs)
+    model.summary()
+
+    ###############
+    # Train Model #
+    ###############
     model.compile(loss=losses.CategoricalCrossentropy(), optimizer=optimizers.Adam(lr=1e-4), metrics=['accuracy'])
     history = model.fit(x_train1, y_train1, epochs=20, batch_size=32, validation_data=(x_test1, y_test1), shuffle=True)
